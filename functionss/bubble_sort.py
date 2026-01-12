ab7cb8dcb2b65e6bbb690553add428cc20266de5
KhushbooMehto <khushboo200022@gmail.com>
2026-01-12 23:38:59 +0530

    1D to 2D start

--- functionss/bubble_sort.py
+++ functionss/bubble_sort.py
@@ -1,13 +1,21 @@
+# in-built sorting ...... internally use Timsort...
+arr=[3,6,9,4,6,2]
+# arr.sort()
+result=sorted(arr)
+print(result)
+
+
+#.........bubble sort...............
 def matrixx(lst):
     
     for i in range(len(lst)-1,0,-1):
         for j in range(i):
-            print(i,j)
+            # print(i,j)
             if lst[j]>lst[j+1]:
-                temp=lst[j]
-                lst[j]=lst[j+1]
-                lst[j+1]=temp
-    
+                lst[j],lst[j+1]=lst[j+1],lst[j]
     return(lst)
         
 print(matrixx([3,4,5,2,8,9,1,6,7]))
+
+#2D 1D
+
